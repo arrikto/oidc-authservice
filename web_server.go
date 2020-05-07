386f93ccf4d666bb42b8a874767cfd68eead93af
Yannis Zarkadas <yanniszark@arrikto.com>
2020-05-07 19:32:43 +0300

    web-server: Add a web server for AuthService's pages
    The AuthService is a server that implements Envoy's ext_authz filter.
    https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/security/ext_authz_filter#arch-overview-ext-authz
    
    In a nutshell, the AuthService exposes a server on which Envoy will proxy
    requests (headers only) to ask if they are allowed or not. A response of 200 OK
    means the user's request will be proxied upstream. Any other response is sent to
    the user as is.
    
    Up until now, we have implemented the AuthService's API endpoints (oidc callback,
    logout) on this server. These endpoints work because the return other-than-200
    responses (they redirect), so the response is sent to the user. However, now we
    want to serve some HTML pages (homepage, after_logout page) and we can't do it
    on the same server as we can't send a 200 OK response (Envoy would proxy the
    request and not return our response).
    
    This made us realize that we are mixing up two different interfaces, the one
    facing Envoy and the one facing the end-user. This is why, for our HTML pages
    (and for all user-accessible endpoints) we introduce a web server, living
    alongside the main AuthService server. Right now, the web-server only includes
    the HTML pages, but the plan is to get all user-accessible endpoints under it.
    
    The web server includes a homepage, after_logout page and theme endpoints. In
    addition, we have added a document explaining how a user can create their own
    templates and themes.
    
    The web server listens at port 8082 by default.
    
    Github-PR: #22
    
    Signed-off-by: Yannis Zarkadas <yanniszark@arrikto.com>
    Reviewed-by: Dimitris Aragiorgis <dimara@arrikto.com>

---
+++ web_server.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	tmplLanding     = "homepage.html"
+	tmplAfterLogout = "after_logout.html"
+)
+
+var (
+	HomepagePath    = "/site/homepage"
+	AfterLogoutPath = "/site/after_logout"
+	ThemesPath      = "/site/themes"
+)
+
+type WebServer struct {
+	TemplatePaths []string
+	// Frontend-related values for context
+	ProviderURL string
+	ClientName  string
+	ThemeURL    string
+	Frontend    map[string]string
+}
+
+func (s *WebServer) Start(addr string) error {
+
+	// Start web server
+	// Load templates
+	filenames := []string{}
+	for _, p := range s.TemplatePaths {
+		tmpls, err := listTemplates(p)
+		if err != nil {
+			return err
+		}
+		filenames = append(filenames, tmpls...)
+	}
+
+	// Add functions to context
+	funcs := map[string]interface{}{
+		"resolve_url_ref": func(url, ref string) string {
+			return mustParseURL(url).ResolveReference(mustParseURL(ref)).String()
+		},
+	}
+
+	templates, err := template.New("").Funcs(funcs).ParseFiles(filenames...)
+	if err != nil {
+		return err
+	}
+
+	router := mux.NewRouter()
+
+	data := struct {
+		// Frontend-related values for context
+		Frontend map[string]string
+		// OIDC-related settings
+		ProviderURL string
+		ThemeURL    string
+		ClientName  string
+	}{
+		Frontend:    s.Frontend,
+		ProviderURL: s.ProviderURL,
+		ThemeURL:    s.ThemeURL,
+		ClientName:  s.ClientName,
+	}
+	router.HandleFunc(HomepagePath, siteHandler(templates.Lookup(tmplLanding), data)).Methods(http.MethodGet)
+	router.HandleFunc(AfterLogoutPath, siteHandler(templates.Lookup(tmplAfterLogout), data)).Methods(http.MethodGet)
+
+	// Themes
+	router.
+		PathPrefix(ThemesPath).
+		Handler(
+			http.StripPrefix(
+				ThemesPath,
+				http.FileServer(http.Dir("web/themes")),
+			),
+		)
+
+	return http.ListenAndServe(addr, handlers.CORS()(router))
+}
+
+// siteHandler returns an http.HandlerFunc that serves a given template
+func siteHandler(tmpl *template.Template, data interface{}) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger := loggerForRequest(r)
+		if err := tmpl.Execute(w, data); err != nil {
+			logger.Errorf("Error executing template: %v", err)
+		}
+	}
+}
+
+func listTemplates(dir string) ([]string, error) {
+	tmplPaths := []string{}
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, f := range files {
+		if !f.IsDir() && strings.HasSuffix(f.Name(), ".html") {
+			tmplPaths = append(tmplPaths, filepath.Join(dir, f.Name()))
+		}
+	}
+	return tmplPaths, err
+}
