65f1d6bcdfb249e10f132be5d01276470c5edb28
Adam Setters <9741640+asetty@users.noreply.github.com>
2021-03-16 15:06:13 -0400

    settings: Add LOG_LEVEL env var to set log level

--- settings.go
+++ settings.go
@@ -64,6 +64,9 @@ type config struct {
 	// Authorization
 	GroupsAllowlist []string `split_words:"true" default:"*"`
 	AuthzConfigPath string   `split_words:"true"`
+
+	// Other
+	LogLevel string `split_words:"true" default:"info"`
 }
 
 func parseConfig() (*config, error) {
