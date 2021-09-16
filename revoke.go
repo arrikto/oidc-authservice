abde900329d995bebbeaa69f95cfd4d33369d5ce
Athanasios Markou <athamark@arrikto.com>
2021-09-16 19:23:43 +0300

    test: test commit
    check how to submit a PR in a public opensource repo.
    
    Signed-off-by: Athanasios Markou athamark@arrikto.com

--- revoke.go
+++ revoke.go
@@ -8,6 +8,7 @@ import (
 	"net/url"
 	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/coreos/go-oidc"
 	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
@@ -31,16 +32,21 @@ func revocationEndpoint(p *oidc.Provider) (string, error) {
 // revokeTokens is a helper that takes an oauth2.Token and revokes the access and refresh tokens.
 // If no tokens are found, it succeeds.
 func revokeTokens(ctx context.Context, revocationEndpoint string, token *oauth2.Token, clientID, clientSecret string) error {
+	if token.RefreshToken != "" {
+		log.Info("Attempting to revoke refresh token...")
+		err := revokeToken(ctx, revocationEndpoint, token.RefreshToken, "refresh_token", clientID, clientSecret)
+		if err != nil {		
+			return errors.Wrap(err, "Failed to revoke refresh token")
+		}
+		log.Info("Successfully revoked refresh token.")
+	}
 	if token.AccessToken != "" {
+		log.Info("Attempting to revoke access token...")
 		err := revokeToken(ctx, revocationEndpoint, token.AccessToken, "access_token", clientID, clientSecret)
 		if err != nil {
-			return errors.Wrap(err, "Failed to revoke access token")
-		}
-	}
-	if token.RefreshToken != "" {
-		err := revokeToken(ctx, revocationEndpoint, token.RefreshToken, "refresh_token", clientID, clientSecret)
-		if err != nil {
-			return errors.Wrap(err, "Failed to revoke refresh token")
+			log.Warning("Failed to revoke access token")
+		} else {
+			log.Info("Successfully revoked access token.")
 		}
 	}
 	return nil
